2dfe32925e26c43c38b3ed1c296204daf0d833ad
Dii <dijiang@borderxlab.com>
2020-06-09 18:26:02 +0800

    add password page

---
+++ lib/views/login/password_change/password_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import 'package:fluro/fluro.dart';
+
+import 'package:flutter_wechat/constant/constant.dart';
+
+import 'package:flutter_wechat/routers/routers.dart';
+
+class ResetPasswordPage extends StatefulWidget {
+  ResetPasswordPage({Key key}) : super(key: key);
+  _ResetState.createState() => _ResetState();
+}
+class _ResetState extends State <CurrentLoginPage> {
+
+}
